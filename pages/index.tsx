c49fbe2fc756ca6845a915d0e31ff4a3adcc704d
Chenchunaidu <maddinenichenchunaidu@gmail.com>
2022-03-21 14:54:19 +0530

    changed path to home

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,17 @@
-import type { NextPage } from "next";
-import styles from "../styles/Home.module.css";
+// import type { NextPage } from "next";
+// import styles from "../styles/Home.module.css";
 
-const Home: NextPage = () => {
-  return <div className={styles.container}></div>;
-};
+// const Home: NextPage = () => {
+//   return <div className={styles.container}></div>;
+// };
 
-export default Home;
+// export default Home;
+import React from "react";
+import EditorComp from "../components/Editor/EditorComp";
+
+// structure
+// should contain text, direction, component,component quantity, image
+
+export default function Editor() {
+  return <EditorComp />;
+}
